cf893cf43deddf797e16196bf8d49ad2e55a4fb2
Jeffrey Day <jeffday1113@gmail.com>
2015-02-04 05:15:20 -0500

    Basic groups implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'groups/index'
+
+  get 'groups/show'
+
+  get 'groups/new'
+
+  get 'groups/edit'
+
+  get 'groups/delete'
+
   devise_for :users
   resources :events
+  resources :groups
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
