88e69c2dd2692aba4170f2f66a21907a38042503
Brian Bolze <brianbolze@gmail.com>
2015-02-17 00:50:42 -0500

    changed root route to login and calendar pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,17 @@ Rails.application.routes.draw do
   get 'groups/delete'
 
   devise_for :users
+  
+  devise_scope :user do
+    authenticated :user do
+      root 'events#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  
   resources :events
   resources :groups
 
@@ -17,7 +28,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
