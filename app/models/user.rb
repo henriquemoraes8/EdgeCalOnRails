7027b2916928d1ca4e36e8677f14c08c47980b40
Henrique Moraes <hrm8@duke.edu>
2015-02-18 05:35:28 -0500

    user can subscribe and unsubscribe freely to other users events

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,20 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :email, :encrypted_password
 
   def has_subscription_to_event(event_id)
-    puts "GOT TO METHOD"
-    return !(self.subscribed_events.find(event_id).nil?)
+    puts "GOT TO METHOD, WILL RETURN, USER ID #{self.id} EVENT ID #{event_id} #{!(self.subscribed_events.find_by_id(event_id).nil?)}"
+    return !(self.subscribed_events.find_by_id(event_id).nil?)
+  end
+
+  def subscribe_to_event(event)
+    if Subscription.where(subscribed_event_id: event.id,subscriber_id: self.id).empty?
+      Subscription.create(subscribed_event_id: event.id,subscriber_id: self.id)
+    end
+  end
+
+  def unsubscribe_to_event(event)
+    if !Subscription.where(subscribed_event_id: event.id,subscriber_id: self.id).empty?
+      Subscription.where(subscribed_event_id: event.id,subscriber_id: self.id).first.destroy
+    end
   end
   
 end
