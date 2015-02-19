ea4d38d6a03e37fb06f19559ca671366247f1be8
Henrique Moraes <hrm8@duke.edu>
2015-02-19 16:21:50 -0500

    better visualize events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,19 +4,10 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @subscription = Subscription.where('subscriber_id = ?',current_user.id)
-
-    #Subscription.where('subscriber_id = ? or visibility IN(?)',current_user.id,[Subscription.visibilities[:busy],
-                                                                                                #Subscription.visibilities[:visible],
-                                                                                                #Subscription.visibilities[:modify]])
-    @Events_id_array = []
-    @subscription.each do |subscript|
-      @Events_id_array << subscript.subscribed_event_id
-    end
-
-    @events = Event.where('id in(?)',@Events_id_array)
-    @subscription = Subscription.where(subscribed_event_id: params[:id]).where(subscriber_id: current_user.id).first
-
+    @own_events = current_user.created_events
+    @visible_events = current_user.get_visible_events
+    @modifiable_events = current_user.get_modifiable_events
+    @busy_events = current_user.get_busy_events
 
     @friends = User.all
   end
