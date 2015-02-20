28b8f833c89b31bc167cc4cc5f024fddaa978aa9
Henrique Moraes <hrm8@duke.edu>
2015-02-20 03:23:12 -0500

    setting up to do form

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,14 +1,16 @@
 class ToDosController < ApplicationController
   def index
-    @todos = ToDo
+    @todos = current_user.to_dos
   end
 
   def edit
     @todo = ToDo.find(params[:id])
+    @todo_count = current_user.to_dos.count
   end
 
   def new
     @todo = ToDo.new
+    @todo_count = current_user.to_dos.count + 1
   end
 
   def create
