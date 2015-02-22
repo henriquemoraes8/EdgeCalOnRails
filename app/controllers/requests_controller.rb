ba4e57d4139686b7f11529cfd523240cec3fda87
Henrique Moraes <hrm8@duke.edu>
2015-02-22 13:49:03 -0500

    edit and new, but not tested

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -9,18 +9,28 @@ class RequestsController < ApplicationController
 
   def edit
     @request_map = RequestMap.find(params[:id])
+    @groups = current_user.groups
   end
 
   def new
     @request_map = RequestMap.new
+    @groups = current_user.groups
   end
 
   def create
 
   end
 
-  def destroy
+  def update
+    @request_map = RequestMap.find(params[:id])
+  end
 
+  def destroy
+    @request_map = RequestMap.find(params[:id])
+    @request_map
+    flash[:notice] = "Request for Event '#{@request_map.event.title}' deleted successfully"
+
+    redirect_to(requests_index_path)
   end
 
 end
