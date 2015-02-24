41fad46efb21c027956e7062da63cef23c76c4f1
Westen Koorbusch <wes.koorbusch@gmail.com>
2015-02-24 03:14:05 -0500

    Still working on reminders with Rufus...

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -57,14 +57,17 @@ class Reminder < ActiveRecord::Base
   def to_do_reminder
     if recurrence == 'no_recurrence'
       puts "NO RECURR NEXT TIME #{next_reminder_time}"
-      self.job_id = Rufus::Scheduler.singleton.at next_reminder_time do
+      puts "NOW ACCORDING TO RUFUS: #{Rufus::Scheduler.parse(Time.now)}"
+      self.job_id = Rufus::Scheduler.singleton.at next_reminder_time.in_time_zone("Eastern Time (US & Canada)") do
         puts "GONNA SEND"
+        puts "SCHEDULER WOKE UP HERE"
         # NotificationMailer.send_notification_email
         # NotificationMailer.to_do_reminder_email(to_do.creator, to_do).deliver_now
         NotificationMailer.to_do_reminder_email('guy', 'guy2').deliver_now
       end
     else
-      self.job_id = Rufus::Scheduler.singleton.every Reminder.recurrence_to_date_time(recurrence), :first_at => next_reminder_time do
+      self.job_id = Rufus::Scheduler.singleton.every Reminder.recurrence_to_date_time(recurrence), :first_at => next_reminder_time.in_time_zone("Eastern Time (US & Canada)") do
+        puts "SCHEDULER WOKE UP HERE"
         # NotificationMailer.send_notification_email
         # NotificationMailer.to_do_reminder_email(to_do.creator, to_do).deliver_now
         NotificationMailer.to_do_reminder_email('guy', 'guy2').deliver_now
