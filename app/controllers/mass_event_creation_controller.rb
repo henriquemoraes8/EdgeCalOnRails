5f98d36e3c442bd1754ab698bca40b13e530cc42
Henrique Moraes <hrm8@duke.edu>
2015-04-10 05:22:08 -0400

    parsing complete, validating keys and argument number

--- app/controllers/mass_event_creation_controller.rb
+++ app/controllers/mass_event_creation_controller.rb
@@ -4,6 +4,7 @@ class MassEventCreationController < ApplicationController
   TYPE_TO_DO = 'to-do'
   TYPE_REQUEST = 'request'
   TYPE_SLOT = 'slot'
+  ALL_TYPES = [TYPE_REGULAR, TYPE_TO_DO, TYPE_REQUEST, TYPE_SLOT]
 
   def index
     @users = User.where.not(:id => current_user.id)
@@ -29,6 +30,13 @@ class MassEventCreationController < ApplicationController
       return
     end
 
+    validate_input(parsed_params)
+
+    if !@error.blank?
+      render :index
+      return
+    end
+
     render :index
   end
 
@@ -43,12 +51,13 @@ class MassEventCreationController < ApplicationController
     index = 1
     split.each do |s|
       event = s.split(%r{;\s*})
-      key = event.shift.downcase
+      key = event.shift.downcase.strip
 
       key_error = validate_key(key, index)
       @error += key_error
 
       if key_error.blank?
+        index += 1
         next
       end
 
@@ -56,7 +65,7 @@ class MassEventCreationController < ApplicationController
       event.each do |e|
         parameter = e.split(%r{:\s*})
         @error += validate_parameter_length(parameter, index)
-        (event_hash[parameter.first] = parameter.count > 1 ? parameter.second : "") if parameter.count > 0
+        (event_hash[parameter.first.downcase.strip] = parameter.count > 1 ? parameter.second.strip : "") if parameter.count > 0
       end
 
       event_array << event_hash
@@ -76,11 +85,64 @@ class MassEventCreationController < ApplicationController
   end
 
   def validate_key(key, line)
-    if key != TYPE_REGULAR && key != TYPE_REQUEST && key != TYPE_SLOT && key != TYPE_TO_DO
+    puts "VALIDATE KEY >#{key}<"
+    if !(ALL_TYPES.include? key)
       return "event #{line}: invalid event type\n"
     end
     ''
   end
 
+  def validate_input(input_array)
+    line = 1
+    input_array.each do |e|
+      case e[:type]
+        when TYPE_REGULAR
+          validate_regular(e, line)
+        when TYPE_REQUEST
+          validate_request(e, line)
+        when TYPE_TO_DO
+          validate_to_do(e, line)
+        when TYPE_SLOT
+          validate_slot(e, line)
+        else
+      end
+    end
+  end
+
+  def validate_regular(regular_hash, line)
+    validate_number_arguments(regular_hash, line, 3, 5)
+    validate_keys(regular_hash, line, ['title', 'description', 'start', 'end', 'to-do'])
+  end
+
+  def validate_request(request_hash, line)
+    validate_number_arguments(request_hash, line, 4, 6)
+    validate_keys(regular_hash, line, ['title', 'description', 'start', 'end', 'to-do'])
+  end
+
+  def validate_to_do(to_do_hash, line)
+    validate_number_arguments(to_do_hash, line, 3, 4)
+    validate_keys(to_do_hash, line, ['title', 'description', 'start', 'end', 'to-do'])
+  end
+
+  def validate_slot(slot_hash, line)
+    if arguments.count < 6
+      @error += "event #{line}: wrong number of arguments. You provided #{arguments.count} arguments of allowed minimum of 6"
+    end
+    validate_keys(regular_hash, line, ['title', 'description', 'start', 'end', 'to-do'])
+  end
+
+  def validate_number_arguments(arguments, min, max, line)
+    if arguments.count < min + 1 || arguments.count > max + 1
+      @error += "event #{line}: wrong number of arguments. You provided #{arguments.count} arguments of allowed range [#{min}, #{max}]"
+    end
+  end
+
+  def validate_keys(arguments, line, possible_keys)
+    arguments.each do |key, value|
+      if !(possible_keys.include? key)
+        @error += "event #{line}: key #{key} not valid"
+      end
+    end
+  end
 
 end
