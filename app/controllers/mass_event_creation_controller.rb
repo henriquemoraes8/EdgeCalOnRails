31a8e4d865ddc21503a4cb08a689d8e9090adda3
Henrique Moraes <hrm8@duke.edu>
2015-04-10 03:45:31 -0400

    mass page ready

--- app/controllers/mass_event_creation_controller.rb
+++ app/controllers/mass_event_creation_controller.rb
@@ -1,11 +1,16 @@
 class MassEventCreationController < ApplicationController
   def index
-
+    @users = User.where.not(:id => current_user.id)
+    @groups = current_user.groups
   end
 
   def help
   end
 
   def show
+  end
+
+  def process_events
+
   end
 end
