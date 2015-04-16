bb4d8e4b6560a305c52e7e4b0dbfa7793b69e6f8
Westen Koorbusch <wes.koorbusch@gmail.com>
2015-04-15 22:49:11 -0400

    User can now get an email with all their events

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,30 @@ class NotificationMailer < ApplicationMailer
 
   require 'mailgun'
 
+  def send_html_email(user, html)
+    @user = user
+    puts "USER EMAIL IS: " + user.email
+
+    mg_client = Mailgun::Client.new "key-345efdd486ec59509f9161b99b78d333"
+
+    # Define your message parameters
+    puts "SENDING EMAIL HERE"
+    message_params = {:from => 'notifications@sandboxb478b65d1ad94458945aa2e6e6549bba.mailgun.org',  
+      :to      => user.email, #'wes.koorbusch@gmail.com',
+      :subject => 'Your Events!',
+      :html    => html
+    }
+
+    mg_client.send_message "sandboxb478b65d1ad94458945aa2e6e6549bba.mailgun.org", message_params
+
+
+    # get_visible_events
+    # get_busy_events
+    # get_modifiable_events
+    # get_requested_events
+    # get_time_slot_created_events
+  end
+
   def notification_email(user)
     @user = user
     mail(to: @user.email, subject: 'Notification Email')
@@ -19,6 +43,24 @@ class NotificationMailer < ApplicationMailer
       :to      => user.email, #'wes.koorbusch@gmail.com',
       :subject => 'To-Do Reminder!',
       :text    => 'This is a reminder to check your to-do list!',
+    }
+
+    mg_client.send_message "sandboxb478b65d1ad94458945aa2e6e6549bba.mailgun.org", message_params
+
+  end
+
+  def to_do_reminder_email(user, to_do)
+    @user = user
+    @to_do = to_do
+    puts "USER EMAIL IS: " + user.email
+
+    mg_client = Mailgun::Client.new "key-345efdd486ec59509f9161b99b78d333"
+
+    # Define your message parameters
+    message_params = {:from => 'notifications@sandboxb478b65d1ad94458945aa2e6e6549bba.mailgun.org',  
+      :to      => user.email, #'wes.koorbusch@gmail.com',
+      :subject => 'To-Do Reminder!',
+      :html    => 'notofication_mailer/notification_email.html.erb',
     }
 
     mg_client.send_message "sandboxb478b65d1ad94458945aa2e6e6549bba.mailgun.org", message_params
